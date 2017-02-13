ed3d020af224b3a5a4619b11ff52fc994d63d3f9
Johan Carlquist <jocar@su.se>
2017-02-13 14:58:50 +0100

    Initial commit.

---
+++ Casks/printomat2.rb
@@ -0,0 +1,10 @@
+cask 'printomat2' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.rymdvarel.se/tmp/Printomat2.pkg'
+  name 'Printomat2'
+  homepage 'https://zero.comaround.com/link/2fe925027240400c8837e5a0e2c9b9a9/'
+
+  pkg 'Printomat2.pkg', allow_untrusted: true
+end
