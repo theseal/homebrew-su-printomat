2956c4667c3ea696c4d2b5fd2e8f8d8c31f3f5e2
Johan Carlquist <jocar@su.se>
2017-02-13 15:27:30 +0100

    Don't relay on my web server for the download.
    The offical download source doesn't allow downloads without using cookies.
    That ayn't good.
    
    Its not good to use my personal webserver either.
    
    Try to use github as source.

--- Casks/printomat2.rb
+++ Casks/printomat2.rb
@@ -2,7 +2,7 @@ cask 'printomat2' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.rymdvarel.se/tmp/Printomat2.pkg'
+  url 'https://github.com/theseal/homebrew-su-printomat/raw/bin/assets/Printomat2.pkg'
   name 'Printomat2'
   homepage 'https://zero.comaround.com/link/2fe925027240400c8837e5a0e2c9b9a9/'
 
